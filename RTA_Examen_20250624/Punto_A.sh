fd17c2813878fa2511df68982427e2a29de6d3f4
Cristian Genovese <78283083+Cristiangenovese@users.noreply.github.com>
2025-07-08 16:03:08 -0300

    Update Punto_A.sh

--- RTA_Examen_20250624/Punto_A.sh
+++ RTA_Examen_20250624/Punto_A.sh
@@ -65,6 +65,7 @@ echo ">>> Creando VG y LV para swap..."
 sudo vgcreate vg_temp /dev/sdc1
 sudo lvcreate -n lv_swap -l 100%FREE vg_temp
 sudo mkswap /dev/mapper/vg_temp-lv_swap
+sudo vgchange -ay
 sudo swapon /dev/mapper/vg_temp-lv_swap
 sleep 1
 
@@ -78,8 +79,6 @@ sudo vgcreate vg_datos /dev/sdd1 /dev/sde1
 sudo lvcreate -L 10M -n lv_docker vg_datos
 sudo lvcreate -L 1.5G -n lv_multimedia vg_datos
 sleep 2
-
-sudo vgchange -ay
 
 echo ">>> Formateando volúmenes..."
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
